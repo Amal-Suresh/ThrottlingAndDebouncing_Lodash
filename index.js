60a3bcaacd9129338e40575c218b07efc794af01
Amal-Suresh <amalsureshms2001@gmail.com>
2023-10-28 22:23:02 +0530

    throttling with lodash

--- index.js
+++ index.js
@@ -10,8 +10,13 @@ const debouncedCount =_.debounce(()=>{
     incT.innerHTML=++trigerCount
 },800) 
 
+//throttling
+const throttlCount = _.throttle(()=>{
+    incT.innerHTML=++trigerCount
+},1000)
+
 
 btn.addEventListener('click',()=>{
     btnP.innerHTML=++pressCount
-    debouncedCount()
+    throttlCount()
 })
